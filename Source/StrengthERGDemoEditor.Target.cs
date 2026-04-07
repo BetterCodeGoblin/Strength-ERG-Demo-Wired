fd4db9c32b37d13ffd96e99f194b71c14ee0f266
BetterCodeGoblin <111158466+BetterCodeGoblin@users.noreply.github.com>
2026-04-07 10:32:03 -0700

    feat: add UE5 C++ Source/StrengthERGDemoEditor.Target.cs

---
+++ Source/StrengthERGDemoEditor.Target.cs
@@ -0,0 +1,13 @@
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class StrengthERGDemoEditorTarget : TargetRules
+{
+	public StrengthERGDemoEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V3;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5Latest;
+		ExtraModuleNames.Add("StrengthERGDemo");
+	}
+}

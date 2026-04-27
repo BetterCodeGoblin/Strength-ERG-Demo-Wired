91c5aa0f56b0da464114fc1203c330de38b5e8b0
Kato <sypherdj1@gmail.com>
2026-04-27 14:28:37 -0700

    fix: add ue5 target rules and update project metadata

---
+++ UE5/Source/StrengthERGEditor.Target.cs
@@ -0,0 +1,13 @@
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class StrengthERGEditorTarget : TargetRules
+{
+    public StrengthERGEditorTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Editor;
+        DefaultBuildSettings = BuildSettingsVersion.V5;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+        ExtraModuleNames.Add("StrengthERG");
+    }
+}
